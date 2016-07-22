9395d1686fbcde3398c0f71261500d831a54298f
gaa7par <gaa7par@gmail.com>
2016-07-22 13:07:32 +0200

    Add rooms list and rooms form

--- app/controllers/admin/rooms_controller.rb
+++ app/controllers/admin/rooms_controller.rb
@@ -1,5 +1,10 @@
 class Admin::RoomsController < ApplicationController
   def index
+    @rooms = Room.all
+  end
+
+  def show
+    @room = Room.find(params[:id])
   end
 
   def new
@@ -7,6 +12,14 @@ class Admin::RoomsController < ApplicationController
   end
 
   def create
-    render plain: params[:room].inspect
+    @room = Room.new(room_params)
+
+    @room.save
+    redirect_to [:admin, @room]
+  end
+
+  private
+  def room_params
+    params.require(:room).permit(:name)
   end
 end
