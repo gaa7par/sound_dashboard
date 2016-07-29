c3d7a6e3d685459f1f82e87c5ae91639a513e1e0
gaa7par <gaa7par@gmail.com>
2016-07-29 11:15:07 +0200

    Add random method to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,26 +1,49 @@
-Admin.create!(email: "admin@example.com",
-  password: "admin1", password_confirmation: "admin1")
+Admin.create!(email: "admin@example.com", password: "admin1", password_confirmation: "admin1")
 
 names = ["Master Builders", "Other Room", "Yet Another"]
 
-date = [2016, 07, 27]
-period = 5
+Date = [2016, 07, 28]
+Period = 5
 
-names.each do |name|
-  unless Room.find_by(name: name)
-    room = Room.create(name: name)
+def random
+  random = rand(3)
+  if random == 0
+    rand(11)
+  elsif random == 1
+    6
+  else
+    7
+  end
+end
 
-    (1..3).each do |number|
-      recorder = room.recorders.create(name: "#{room.id}.#{number}")
+def add_rooms(names)
+  names.each do |name|
+    unless Room.find_by(name: name)
+      room = Room.create(name: name)
+      add_recorders(room)
+    end
+  end
+end
 
-      (8..15).each do |hour|
-        (0..59).each do |minute|
-          if minute % period == 0
-            time = Time.new(*date, hour, minute)
-            recorder.measurements.create!(measure: rand(11), time: time)
-          end
-        end
+def add_recorders(room)
+  (1..3).each do |number|
+    recorder = room.recorders.create(name: "#{room.id}.#{number}")
+    add_measurements(recorder)
+
+    time = Time.new(*Date, 16)
+    recorder.measurements.create!(measure: random, time: time)
+  end
+end
+
+def add_measurements(recorder)
+  (8..15).each do |hour|
+    (0..59).each do |minute|
+      if minute % Period == 0
+        time = Time.new(*Date, hour, minute)
+        recorder.measurements.create!(measure: random, time: time)
       end
     end
   end
 end
+
+add_rooms(names)
