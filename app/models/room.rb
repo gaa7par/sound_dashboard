09721efdc585ca092dde660707da4773f840be78
gaa7par <gaa7par@gmail.com>
2016-08-03 16:01:46 +0200

    Add logic to Room model

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,4 +1,15 @@
 class Room < ApplicationRecord
   has_many :recorders, dependent: :destroy
   validates :name, presence: true, uniqueness: true, length: { maximum: 20 }
+
+  def measures_for_current_day_chart
+    measures = Hash.new(0)
+
+    recorders.each do |recorder|
+      recorder.measurements.each do |measurement|
+        measures[measurement.time.to_i] += measurement.measure
+      end
+    end
+    measures.values.map { |n| n / 3 }
+  end
 end
