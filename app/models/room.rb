6dc614f59e104fb23d935061a7a049ae9a8530d1
gaa7par <gaa7par@gmail.com>
2016-08-04 11:02:54 +0200

    Add working chart for rooms

--- app/models/room.rb
+++ app/models/room.rb
@@ -4,12 +4,13 @@ class Room < ApplicationRecord
 
   def measures_for_current_day_chart
     measures = Hash.new(0)
+    number_of_recorders = recorders.count
 
     recorders.each do |recorder|
       recorder.measurements.each do |measurement|
         measures[measurement.time.to_i] += measurement.measure
       end
     end
-    measures.values.map { |n| n / 3 }
+    (measures.values.map { |measure| measure / number_of_recorders }).to_json
   end
 end
