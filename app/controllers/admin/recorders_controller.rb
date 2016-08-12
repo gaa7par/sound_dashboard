9b0dcdbb0446912c08bddf4ff8591023427a77fb
gaa7par <gaa7par@gmail.com>
2016-08-12 14:20:40 +0200

    Validate recorders

--- app/controllers/admin/recorders_controller.rb
+++ app/controllers/admin/recorders_controller.rb
@@ -1,12 +1,18 @@
 class Admin::RecordersController < ApplicationController
+  before_action :get_room
+
   def create
-    @room = Room.find(params[:room_id])
-    @recorder = @room.recorders.create(recorder_params)
-    redirect_to admin_room_path(@room)
+    @recorder = @room.recorders.new(recorder_params)
+    if @recorder.save
+      redirect_to admin_room_path(@room), notice: 'Recorder created successfully'
+    else
+      @rooms = Room.all
+      @recorders = @room.recorders.where.not(id: nil)
+      render '/admin/rooms/show'
+    end
   end
 
   def destroy
-    @room = Room.find(params[:room_id])
     @recorder = @room.recorders.find(params[:id])
     @recorder.destroy
 
@@ -14,7 +20,12 @@ class Admin::RecordersController < ApplicationController
   end
 
   private
+
   def recorder_params
     params.require(:recorder).permit(:name, :position_x, :position_y)
+  end
+
+  def get_room
+    @room = Room.find(params[:room_id])
   end
 end
--- app/models/recorder.rb
+++ app/models/recorder.rb
@@ -3,5 +3,6 @@ class Recorder < ApplicationRecord
   has_many :measurements, dependent: :destroy
 
   validates :name, presence: true, uniqueness: true
-  validates :position_x, :position_y, presence: true, numericality: { greater_than: 0 }
+  validates :position_x, presence: true, numericality: { greater_than: 0, less_than_or_equal_to: Proc.new { |r| r.room.dimension_x } }
+  validates :position_y, presence: true, numericality: { greater_than: 0, less_than_or_equal_to: Proc.new { |r| r.room.dimension_y } }
 end
